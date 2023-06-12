48a53169262eabdebcc8a32b51294f7c6af4f9b8
mntzd <julien.leske@gmail.com>
2023-06-12 11:45:45 +0200

    get works

---
+++ db/models/Place.js
@@ -0,0 +1,17 @@
+import mongoose, { mongo } from "mongoose";
+
+// destructure the Schema Object from mongoose
+const { Schema } = mongoose;
+
+// define the Schema (according to the input data we got from project)
+const placeSchema = new Schema({
+  id: { type: Number, required: true },
+  location: { type: String, required: true },
+  mapURL: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+// define Place with Place if ecist already, otherwise PlacesSchema
+const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
+
+export default Place;
--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,27 @@
-import { places } from '../../../../lib/db.js';
+// --- import the Schema & Connection Function---
+import Place from "/db/models/Place";
+import dbConnect from "/db/connect";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  // Connect to DB and get Slug/ID
+  await dbConnect();
   const { id } = request.query;
 
   if (!id) {
     return;
   }
 
-  const place = places.find((place) => place.id === id);
+  //Defining GET APIroute
+  if (request.method === "GET") {
+    // Assign the place with corresponding id to place
+    const place = await Place.findById(id);
 
-  if (!place) {
-    return response.status(404).json({ status: 'Not found' });
+    // nothing loaded?
+    if (!place) {
+      return response.status(404).json({ error: "nothing found :(" });
+    }
+
+    // successfully loaded?
+    return response.status(200).json(place);
   }
-
-  response.status(200).json(place);
 }
